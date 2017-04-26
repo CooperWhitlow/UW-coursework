99e77ace0dbfeae07f18712224833a811838a529
Cooper Whitlow <cooperwhitlow@gmail.com>
2017-04-26 07:11:44 -0400

    began adding POI capabilities

---
+++ hw1-maps/hw1-maps/PinPoint.swift
@@ -0,0 +1,13 @@
+//
+//  PinPoint.swift
+//  hw1-maps
+//
+//  Created by Cooper Whitlow on 4/24/17.
+//  Copyright © 2017 Cooper Whitlow. All rights reserved.
+//
+
+import UIKit
+
+class PinPoint: NSObject {
+
+}
